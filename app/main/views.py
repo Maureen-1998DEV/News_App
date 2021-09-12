6948b4097204d418e4a099eb49036a1d7af47eeb
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-09-12 22:30:20 +0300

    Added  index.css

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
+
 from ..request import get_newsource,get_articles,search_article
 
 #views
--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-export NEWS_API_KEY =
+export NEWS_API_KEY ='409c0e6afcce4ba0b4749a6ea62c1ce3'
+
+python3.8 manage.py server
